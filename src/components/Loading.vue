e4598470d90a069cbf2ce9b41b76215ce134e637
licheng1013 <2501093733@qq.com>
2022-10-10 11:21:37 +0800

    load组件已完善！

--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -1,7 +1,7 @@
 <template>
   <div v-if="loadingFlag" class="loading">
     <div>
-      <el-image class="animate__infinite" :class="animBounce" :src="url" style="width: 50px;height:50px">
+      <el-image  :src="url" style="width: 300px;height:300px">
       </el-image>
     </div>
   </div>
@@ -9,8 +9,7 @@
 
 <script setup>
 // 一个简单的loading组件,您也可以把他当作全局loading来使用
-import {animBounce} from "@/assets/anim";
-import imgUrl from '@/assets/load.png'
+import imgUrl from '@/assets/loading.gif'
 import {ref} from "vue";
 const url = ref(`${imgUrl}`)
 defineProps({loadingFlag: Boolean})
@@ -23,7 +22,7 @@ defineProps({loadingFlag: Boolean})
   height: 100%;
   width: 100%;
   z-index: 1;
-  background-color: rgba(117, 106, 106, 0.5);
+  background-color: rgba(0, 0, 0, 0.5);
 
   display: flex;
   align-items: center;
